2bad982ccbf98c8eed2d90a60c4aef0cd8bdd5ed
YuliiaMakhovska <y.makhovska@gmail.com>
2022-10-01 12:37:30 +0300

    add second and third tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const LOCALKEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+function onTimeUpdate(e) {
+  localStorage.setItem(LOCALKEY, e.seconds);
+}
+
+const savedTime = localStorage.getItem(LOCALKEY);
+if (savedTime) {
+  const currentTime = JSON.parse(savedTime);
+  player.setCurrentTime(currentTime);
+}
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+// player.setCurrentTime(30.456).then(function(seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('input');
+const messageEl = document.querySelector('textarea');
 
+form.addEventListener('input', throttle(onInputChange, 500));
+form.addEventListener('submit', onFormSubmit);
+const feedbackFormState = localStorage.getItem('feedback-form-state')
+  ? JSON.parse(localStorage.getItem('feedback-form-state'))
+  : {};
+onSavedMessageInput();
+function onInputChange(e) {
+  feedbackFormState[e.target.name] = e.target.value;
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(feedbackFormState)
+  );
+  console.log(feedbackFormState);
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  if (emailEl.value === '' || messageEl.value === '') {
+    alert('Please fill in the empty fields');
+    return;
+  }
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function onSavedMessageInput() {
+  const savedMessage = localStorage.getItem('feedback-form-state');
+  if (savedMessage) {
+    if (savedMessage.message) {
+      messageEl.textarea.value = savedMessage.message;
+    }
+    if (savedMessage) {
+      if (savedMessage.email) {
+        emailEl.value = savedMessage.email;
+      }
+    }
+  }
+  console.log(savedMessage);
+}
