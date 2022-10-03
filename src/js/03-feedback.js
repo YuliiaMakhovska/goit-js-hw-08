3c0c8a1ff82ef408b407ad04fcd2abe0b500ee7b
YuliiaMakhovska <y.makhovska@gmail.com>
2022-10-03 11:25:35 +0300

    add changes in third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,37 +8,36 @@ form.addEventListener('submit', onFormSubmit);
 const feedbackFormState = localStorage.getItem('feedback-form-state')
   ? JSON.parse(localStorage.getItem('feedback-form-state'))
   : {};
-onSavedMessageInput();
+populateTextarea();
 function onInputChange(e) {
   feedbackFormState[e.target.name] = e.target.value;
   localStorage.setItem(
     'feedback-form-state',
     JSON.stringify(feedbackFormState)
   );
-  console.log(feedbackFormState);
+  // console.log(feedbackFormState);
 }
 
 function onFormSubmit(e) {
   e.preventDefault();
+  const form = e.currentTarget;
+  const feedbackFormState = new FormState(form);
+  const updateForm = {};
   if (emailEl.value === '' || messageEl.value === '') {
     alert('Please fill in the empty fields');
     return;
   }
+  updateForm[key] = value;
+  console.log(updateForm);
   e.currentTarget.reset();
   localStorage.removeItem('feedback-form-state');
 }
 
-function onSavedMessageInput() {
-  const savedMessage = localStorage.getItem('feedback-form-state');
+function populateTextarea() {
+  const savedMessage = JSON.parse(localStorage.getItem('feedback-form-state'));
   if (savedMessage) {
-    if (savedMessage.message) {
-      messageEl.textarea.value = savedMessage.message;
-    }
-    if (savedMessage) {
-      if (savedMessage.email) {
-        emailEl.value = savedMessage.email;
-      }
-    }
+    // console.log(savedMessage);
+    emailEl.value = savedMessage['email'] || '';
+    messageEl.value = savedMessage['message'] || '';
   }
-  console.log(savedMessage);
 }
